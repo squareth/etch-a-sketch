bd2c4da35537ad6937688358bfd0a7899458e219
Squareth <pesticidek@hotmail.com>
2022-07-02 21:37:26 +0200

    Create createGrid function

---
+++ script.js
@@ -0,0 +1,12 @@
+
+mainContainer = document.querySelector("#main-container");
+createGrid(16);
+
+function createGrid(amount){
+    mainContainer.style.gridTemplateColumns = "repeat(" + amount + ", 1fr)";
+    for(i = 0; i < (amount * amount); i++){
+        let square = document.createElement('div');
+        square.className = "etch-pixel";
+        mainContainer.appendChild(square);
+    }
+}
